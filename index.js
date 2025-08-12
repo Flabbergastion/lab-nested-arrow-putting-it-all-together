55a8f06388ad0f03a34f54d8b0a001a304c17e8a
Flabbergastion <maxgralpois@gmail.com>
2025-08-12 14:17:45 -0700

    Issue with tracking login count

--- index.js
+++ index.js
@@ -4,3 +4,37 @@
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+function createLoginTracker (userInfo, maxAttempts = 3) {
+    
+    
+    let attemptCount = 0;
+    let accountLocked = false;
+
+    const handleLoginAttempt = (passwordAttempt) => {
+      if (accountLocked) {
+        return "Account is locked. Please contact support.";
+      }
+
+      attemptCount++;
+
+      if (passwordAttempt === userInfo.password) {
+        accountLocked = false;
+        attemptCount = 0;
+        return "Login successful";
+      } else {
+        if (attemptCount >= maxAttempts) {
+        accountLocked = true;
+        return "Account locked due to too many failed login attempts";
+        } else {
+          const remainingAttempts = maxAttempts - attemptCount;
+          return 'Attempt 1: Login failed';
+          }      
+        }
+      };
+
+      return handleLoginAttempt;
+
+    }
+
+   
